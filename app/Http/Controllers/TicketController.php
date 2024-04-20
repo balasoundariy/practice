065d38677188f52e3b4800e22e3319c5d81c2ade
Balaji soundariy <balajisoundariy@theswipewire.com>
2024-04-21 01:29:21 +0530

    back work

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -19,18 +19,30 @@ class TicketController extends Controller
 
     public function summary(Request $request)
     {
-        $tickets = new Ticket();
-        $tickets->ticket_amount = $request->ticket_amount;
-        $tickets->ticket_no = json_encode($request->ticket_no);
-        $tickets->chances = json_encode($request->chances);
-        $tickets->total = 1234;
-        $tickets->save();
+        $ticket_id = session()->has('ticket_id')? session()->get('ticket_id'): false;
+        if($ticket_id){
+            $tickets = Ticket::find($ticket_id);
+            $tickets->ticket_amount = $request->ticket_amount;
+            $tickets->ticket_no = json_encode($request->ticket_no);
+            $tickets->chances = json_encode($request->chances);
+            $tickets->total = 123456;
+            $tickets->save();
+        }else{
+            $tickets = new Ticket();
+            $tickets->ticket_amount = $request->ticket_amount;
+            $tickets->ticket_no = json_encode($request->ticket_no);
+            $tickets->chances = json_encode($request->chances);
+            $tickets->total = 1234;
+            $tickets->save();
+            session()->put('ticket_id',$tickets->id);
+        }
         return view('summary',compact('tickets'));
     }
 
     public function makePayment(Request $request)
     {
         //update payment status whether they have paid or not
+        session()->forget('ticket_id');
         return Response()->json([
             'status' => 200,
         ]);
